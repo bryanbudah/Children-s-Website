d48d1c5fd4c255c81ba5002ffcddb3f5262eb133
Bryan Kariuki <webdeveloperjoekariuki@gmail.com>
2026-05-08 08:15:20 +0300

    changed home.html to index.html

--- childrens_home/childrens_home/settings.py
+++ childrens_home/childrens_home/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = "childrens_home.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates'],
+        "DIRS": [BASE_DIR.parent / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
