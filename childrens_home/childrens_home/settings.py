e49d8c8ce7ad6ac9bfbf19082caefc04549668d0
Bryan Kariuki <webdeveloperjoekariuki@gmail.com>
2026-05-29 09:00:19 +0300

    Add static images

--- childrens_home/childrens_home/settings.py
+++ childrens_home/childrens_home/settings.py
@@ -125,9 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-     BASE_DIR / "childrens_home" / "static",
+         BASE_DIR / "static",
 ]
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
